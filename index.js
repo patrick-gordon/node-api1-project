39dbfc81d7358bc033e1a32c52a4f2129d036e0d
patrick-gordon <patrickgordon42@yahoo.com>
2019-09-26 17:24:53 -0500

    added put, delete and post, MVP

--- index.js
+++ index.js
@@ -27,17 +27,21 @@ server.get('/api/users', (req, res) => {
 //POST add a user (NEED MIDDLEWARE TO CONSOLE.LOG!!!)
 server.post('/api/users', (req, res) => {
     console.log(req.body);  //console.log JSON that is being added 
-    const user = req.body //save user to the body
-    db.insert(user)
-    .then(idObj => db.findById(idObj.id))
-    .then(user => {
-        res.status(201).json(user);
+    const { name, bio } = req.body;  
+    if (!name || !bio) { // prevents name or bio being null on backend side
+        res.status
+    }
+    // const user = req.body //save user to the body
+    db.insert({ name, bio }) // can only insert stuff in keys, no extra fields
+    .then(({id }) => db.findById(id))
+        .then(user => {
+            res.status(201).json(user);
     })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json({error: "server error"});
-    })
-})
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({error: "server error"});
+    });
+});
 
 
 
@@ -59,6 +63,48 @@ server.get('/api/users/:id', (req, res) => {
         })
 });
 
+//DELETE
+server.delete('/api/users/:id', (req, res) => {
+    const { id } = req.params;
+    db.remove(id)
+        .then((deleted) => {
+            if (deleted) {
+            res.status(204).end();
+        } else {
+            res.status(404).json({error: "user with id does not exist"});
+        }
+    })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({error: "server error deleted"});
+        });
+});
 
+
+//PUT (update user)
+server.put('/api/users/:id', (req, res) => {
+    const { id } = req.params
+    const { name, bio } = req.body;
+    if (!name && !bio) {
+        res.status(400).json({error: 'Requires some changes'});
+    }
+    db.update(id, { name, bio })
+        .then(updated => {
+            if (updated) {
+                db.findById(id)
+                .then(user => res.status(200).json(user))
+                .catch(err => {
+                    console.log(err);
+                    res.status(500).json({error: "Error retrieving user"});
+                });
+            } else {
+                res.status(404).json({error: "user id not found"})
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({error: "Error updating user"});
+        })
+})
 
 server.listen(4444, () => console.log("server on port 4444"));
