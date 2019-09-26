141894d11d737090dfcc92d5f7e94f746cc1a61c
patrick-gordon <patrickgordon42@yahoo.com>
2019-09-26 12:36:56 -0500

    imports and initial commit

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // implement your API here
+
+//CRUD OPS
+
+//IMPORRTS
+const express = require('express');
+const server = express();
+const db = require("./data/db");
+
+
+//
+server.listen(4444, () => console.log("server on port 4444"));
+
+//ROUTES
+
+//GET
+server.get('/api/users', (req, res) => {
+    console.log(db.find());
+    res.end();
+})
